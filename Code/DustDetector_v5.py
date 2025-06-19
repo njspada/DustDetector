d17384c893c6001fdfd8bb39f7bba09cda38b573
njspada <njspada@ucdavis.edu>
2025-06-18 18:15:21 -0700

    Working basic functionality

--- Code/DustDetector_v5.py
+++ Code/DustDetector_v5.py
@@ -35,27 +35,49 @@ ORIGIN = (0, 30)
 FONT = cv2.FONT_HERSHEY_SIMPLEX
 SCALE = 1
 THICKNESS = 2
-
-def apply_timestamp(request):
-    """Apply a timestamp to the video frame."""
+        
+def calculate_threshold(request):
+    """Convert the frame to grayscale, threshold to black and white, then calculate pixel ratio"""
+    # Get time for visual confirmation of running and framerate
     timestamp = time.strftime("%Y-%m-%d %X")
+    
     with MappedArray(request, "main") as m:
+        
+        # Convert the frame to grayscale
+        gray_frame = cv2.cvtColor(m.array, cv2.COLOR_BGR2GRAY)
+        
+        # Gaussian filtering (blurring for better discrimination
+        blur = cv2.GaussianBlur(gray_frame, (5, 5), 0)
+        
+        # Apply Otsu's binarization
+        _, binary_frame = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+        
+        # Count black pixels (1 is white, 0 is black)
+        black_pixels = binary_frame.size - cv2.countNonZero(binary_frame)
+        
+        # Calculate the ratio
+        if black_pixels > 0:
+            black_to_total_ratio = black_pixels / binary_frame.size
+        else:
+            black_to_total_ratio = 0.0
+        
+        # Display the calculated ratio on the frame
         cv2.putText(m.array, timestamp, ORIGIN, FONT, SCALE, COLOR, THICKNESS)
+        cv2.putText(m.array, f'Ratio: {black_to_total_ratio:.3f}', (30, 60), FONT, SCALE, COLOR, THICKNESS)
+    
 
 def main():
     """Main function to run the DustSensor application."""
 	
 	# Open the video capture device
-	# cap = cv2.VideoCapture(0)
     picam2 = Picamera2()
 	
 	# Check if the camera opened successfully
- 	# if not cap.isOpened():
     if not picam2:
         print("Error: Could not open camera.")
         return
     
-    picam2.pre_callback = apply_timestamp
+    picam2.pre_callback = calculate_threshold
     picam2.start_preview(Preview.QTGL)
 
     preview_config = picam2.create_preview_configuration()
@@ -73,52 +95,12 @@ def main():
 
     #Set metadata values
     picam2.set_controls(controls)
-    time.sleep(5)
     
     while True:
-        # Read a frame from the camera
-        # ret, frame = cap.read()
-        frame = picam2.capture_array()
-        
-        # If the frame was not captured successfully,
-        # break the loop
-        # if not ret:
-        #     print("Error: Could not read frame.")
-        #     break
-        
-        # Convert the frame to grayscale
-        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        
-        # Gaussian filtering (blurring for better discrimination
-        blur = cv2.GaussianBlur(gray_frame, (5, 5), 0)
-        
-        # Apply a binary threshold
-        #_, binary_frame = cv2.threshold(gray_frame, 128, 255, cv2.THRESH_BINARY)
-        # Apply Otsu's binarization
-        _, binary_frame = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-        
-        # Count black pixels (1 is white, 0 is black)
-        black_pixels = binary_frame.size - cv2.countNonZero(binary_frame)
-        
-        # Calculate the ratio
-        if black_pixels > 0:
-            black_to_total_ratio = black_pixels / binary_frame.size
-        else:
-            black_to_total_ratio = 0.0
-        
-        # Display the calculated ratio on the frame
-        cv2.putText(frame, f'Ratio: {black_to_total_ratio:.3f}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-        
-        # Display the captured frame
-        cv2.imshow("Video Stream", frame)
         
         # Check for the 'q' key press to exit the loop
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-	
-	# Release the camera and close the OpenCV windows
-	# cap.release()
-	# cv2.destroyAllWindows()
 	
 if __name__ == "__main__":
 	main()
